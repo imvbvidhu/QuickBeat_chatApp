7efe767c70f0155403ab31886835f944e467bf03
imvbvidhu <imvbvidhu@gmail.com>
2020-04-21 02:15:24 +0530

    first commit

---
+++ client/src/components/TextContainer/TextContainer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import onlineIcon from '../../icons/onlineIcon.png';
+
+import './TextContainer.css';
+
+const TextContainer = ({ users }) => (
+  <div className="textContainer">
+    <div>
+      <h1>Quick Beat Chat Application <span role="img" aria-label="emoji">💬</span></h1>
+      <h3> Creator : Vidhu Bhardwaj </h3>
+    </div>
+    {
+      users
+        ? (
+          <div>
+            <h1>People currently Online:</h1>
+            <div className="activeContainer">
+              <h2>
+                {users.map(({name}) => (
+                  <div key={name} className="activeItem">
+                    {name}
+                    <img alt="Online Icon" src={onlineIcon}/>
+                  </div>
+                ))}
+              </h2>
+            </div>
+          </div>
+        )
+        : null
+    }
+  </div>
+);
+
+export default TextContainer;
